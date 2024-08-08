8e0ab5632ff2a7fdddd3d61cf27e1f220b500c04
chienhoyin <77372981+chienhoyin@users.noreply.github.com>
2024-08-08 09:58:45 -0400

    Update table_2_from_python.py

--- table_preparation/code/table_2_from_python.py
+++ table_preparation/code/table_2_from_python.py
@@ -74,35 +74,6 @@ def get_date_range(sample_df_path):
     
     return date_range
 
-def table_2(f_factor_df,b_factors_df,ff_df,date_range):
-    
-    b_factors_df["ft0"]=f_factor_df["V1"]
-    b_factors_df["date"]=date_range
-    b_factors_df["year"]=pd.to_datetime(b_factors_df["date"]).dt.year
-    b_factors_df["month"]=pd.to_datetime(b_factors_df["date"]).dt.month
-    b_factors_df.drop(columns=["date"],inplace=True)
-    b_factors_df=pd.merge(b_factors_df, ff_factors_monthly, on=["year","month"], how="left", validate="1:1")
-
-    factors=[f"ft{_}" for _ in range(21)]
-    table_cols=["single_sharpe","cum_sharpe","capm_alpha","capm_alpha_t","FF5_alpha","FF5_alpha_t","EF_alpha","EF_alpha_t","EF_R2"]
-    table_2_df=pd.DataFrame(index=factors,columns=table_cols)
-
-    for col in  factors:
-        b_factors_df[col]=b_factors_df[col]-b_factors_df["RF"]
-
-    table_2_df["single_sharpe"]=[Sharpe(b_factors_df[_]) for _ in factors]
-    table_2_df["cum_sharpe"]=[Sharpe(b_factors_df[factors[:_+1]]) for _ in range(len(factors))]
-    table_2_df["capm_alpha"]=[OLS(b_factors_df[_],b_factors_df["Mkt-RF"]).params["const"] for _ in factors]
-    table_2_df["capm_alpha_t"]=[OLS(b_factors_df[_],b_factors_df["Mkt-RF"]).tvalues["const"] for _ in factors]
-    table_2_df["FF5_alpha"]=[OLS(b_factors_df[_],b_factors_df[["Mkt-RF","SMB","HML","RMW","CMA"]]).params["const"] for _ in factors]
-    table_2_df["FF5_alpha_t"]=[OLS(b_factors_df[_],b_factors_df[["Mkt-RF","SMB","HML","RMW","CMA"]]).tvalues["const"] for _ in factors]
-    table_2_df["EF_alpha"]=[OLS(b_factors_df[_],b_factors_df[[f for f in factors if f != _]]).params["const"] for _ in factors]
-    table_2_df["EF_alpha_t"]=[OLS(b_factors_df[_],b_factors_df[[f for f in factors if f != _]]).tvalues["const"] for _ in factors]
-    table_2_df["EF_R2"]=[OLS(b_factors_df[_],b_factors_df[[f for f in factors if f != _]]).rsquared for _ in factors]
-    
-    return table_2_df
-
-
 def table_2_py(factors_df,ff_df):
     
     factors_df["year"]=pd.to_datetime(factors_df.index).year
@@ -163,10 +134,6 @@ if __name__ == "__main__":
         
     ff_factors_monthly=get_ff5(os.path.join("..","..","raw_data","F-F_Research_Data_5_Factors_2x3_adj.csv"))
     
-    #factor_train_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_Boosted_train_factor_19_rf.csv"),index_col=0).sort_index()
-    #table_2_train_df=table_2_py(factor_train_df,ff_factors_monthly)
-    #table_2_train_df.to_csv(os.path.join("..","output","train_table_2_8_4_2024.csv"))    
-    
     factor_train_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_Boosted_train_factor_19_rf.csv"),index_col=0).sort_index()
     factor_train_df.columns=factor_train_df.columns.astype(int)
     
@@ -178,17 +145,3 @@ if __name__ == "__main__":
     table_2_OOS_df.to_csv(os.path.join("..","output","OOS_table_2_8_7_2024.csv"))  
     
     print("stop")
-    '''
-        f_factor_test_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_First_factor_test.csv"))
-        b_factors_test_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_Boosted_factors_test.csv"))
-        date_range_test=get_date_range(os.path.join("..","..","data_preparation","output","weighted_testp.csv"))
-        table_2_test_df=table_2(f_factor_test_df,b_factors_test_df,ff_factors_monthly,date_range_test)
-        table_2_test_df.to_csv(os.path.join("..","output","test_table_2_7_21_2024.csv"))
-        
-        f_factor_oos_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_First_factor_OOS.csv"))
-        b_factors_oos_df=pd.read_csv(os.path.join("..","..","grow_tree","output","Vanilla_Boosted_factors_OOS.csv"))
-        date_range_oos=get_date_range(os.path.join("..","..","data_preparation","output","weighted_testp.csv"))
-        table_2_oos_df=table_2(f_factor_oos_df,b_factors_oos_df,ff_factors_monthly,date_range_oos)
-        table_2_oos_df.to_csv(os.path.join("..","output","OOS_table_2_7_21_2024.csv"))    
-    '''
-    
